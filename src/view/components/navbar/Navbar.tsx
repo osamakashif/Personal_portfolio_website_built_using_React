aeeae6c822b9081d0e054ce15c5f09565557f436
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-05-17 18:44:52 +1200

    [fix]: Name and key.
    Fix name of navbar file, include key for mapped array elements.

--- src/view/components/navbar/Navbar.tsx
+++ src/view/components/navbar/Navbar.tsx
@@ -3,17 +3,17 @@ import "./Navbar.css";
 
 export const Navbar = () => {
   let pages: Page[] = [];
-  pages.push(new Page("About Me", "#About_Me"));
-  pages.push(new Page("Profiles", "#Profiles"));
-  pages.push(new Page("Technological", "#Tech"));
-  pages.push(new Page("Projects", "#Projects"));
+  pages.push(new Page("About Me", "#about_me"));
+  pages.push(new Page("Profiles", "#profiles"));
+  pages.push(new Page("Technological", "#tech"));
+  pages.push(new Page("Projects", "#projects"));
 
   return (
     <nav>
       <ul className="nav-list">
         {pages.map((page) => {
           return (
-            <li>
+            <li key={page.name}>
               <a className="nav-link" href={page.link}>{page.name}</a>
             </li>
           );

d354c6d1130c228ae3554fa05385947e96bf8534
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-05-18 23:49:21 +1200

    [feat]: Sort projects by language.
    Sort projects by language by pressing checkbox.

--- src/view/sections/projects/Projects.tsx
+++ src/view/sections/projects/Projects.tsx
@@ -5,12 +5,15 @@ import { Project } from "../../../model/projects/Project";
 export const Projects = () => {
   const gitHubRepos = "https://api.github.com/users/osamakashif/repos";
 
+  const [sortByLanguage, setSortByLanguage] = useState<boolean>(false);
+  const [languages, setLanguages] = useState<string[]>([]);
   const [projects, setProjects] = useState<Project[]>([]);
   useEffect(() => {
     const getGitHubReposWithFetch = async () => {
       const response = await fetch(gitHubRepos);
       const jsonData = await response.json();
       let newProjects: Project[] = [];
+      let newLanguages: string[] = [];
       Array.from(jsonData).forEach((data: any) => {
         newProjects.push(
           new Project(
@@ -20,8 +23,12 @@ export const Projects = () => {
             data.description
           )
         );
+        if (!newLanguages.includes(data.language)) {
+          newLanguages.push(data.language);
+        }
       });
       setProjects(newProjects);
+      setLanguages(newLanguages);
     };
     getGitHubReposWithFetch();
   }, []);
@@ -30,14 +37,59 @@ export const Projects = () => {
     <div id="projects">
       <h1>Projects</h1>
       <div>
+        <p>Sort by major language</p>
+        <input
+          type="checkbox"
+          onClick={() => {
+            setSortByLanguage(!sortByLanguage);
+          }}
+        ></input>
+      </div>
+      <div>
         {projects &&
-          projects.map((project, index) => (
-            <div key={index}>
-              <a href={project.link} target="_blank" rel="noopener noreferrer">{project.name}</a>
-              <p>{project.mainLanguage}</p>
-              <p>{project.description}</p>
-            </div>
-          ))}
+          !sortByLanguage &&
+          projects.map((project, index) => {
+            return (
+              <div key={index}>
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {project.name}
+                </a>
+                <p>{project.mainLanguage}</p>
+                <p>{project.description}</p>
+              </div>
+            );
+          })}
+        {projects &&
+          sortByLanguage &&
+          languages.map((language) => {
+            return (
+              <div key={language}>
+                <h2>{language}</h2>
+                {projects
+                  .filter((project) => {
+                    return project.mainLanguage === language;
+                  })
+                  .map((project, index) => {
+                    return (
+                      <div key={index}>
+                        <a
+                          href={project.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {project.name}
+                        </a>
+                        <p>{project.description}</p>
+                      </div>
+                    );
+                  })}
+              </div>
+            );
+          })}
       </div>
     </div>
   );

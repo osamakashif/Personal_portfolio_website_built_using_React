2d0653993d8bf8c1c504f85394ba28b004ed65c0
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-05-21 00:38:02 +1200

    [fix]: Correct Navbar options.
    Correct Navbar options.

--- src/view/components/navbar/Navbar.tsx
+++ src/view/components/navbar/Navbar.tsx
@@ -3,10 +3,10 @@ import "./Navbar.css";
 
 export const Navbar = () => {
   let pages: Page[] = [];
+  pages.push(new Page("Home", ""));
   pages.push(new Page("About Me", "#about_me"));
-  pages.push(new Page("Profiles", "#profiles"));
-  pages.push(new Page("Technological", "#tech"));
   pages.push(new Page("Projects", "#projects"));
+  pages.push(new Page("Technological", "#tech"));
 
   return (
     <nav>

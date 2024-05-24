9983577f958394adc1e18830959317486998a28b
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-05-24 12:01:38 +1200

    [feat]: Colour Theme context.
    Colour Theme context with local storage implemented.

---
+++ src/controller/context/ColourThemeContext.tsx
@@ -0,0 +1,55 @@
+import {
+  createContext,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+
+export const ColourThemeContext = createContext({
+  theme: "auto",
+  switchTheme: (newTheme: "auto" | "light" | "dark") => {},
+});
+
+export function ColourThemeContextProvider({ children }: { children: any }) {
+  const [theme, setTheme] = useState<"auto" | "light" | "dark">("auto");
+  const [firstLoad, setFirstLoad] = useState<boolean>(true);
+  const cache_key = "theme";
+
+  useEffect(() => {
+    if (!firstLoad) {
+      localStorage.setItem(cache_key, theme);
+    }
+  }, [theme, firstLoad]);
+
+  useEffect(() => {
+    const stored_value = localStorage.getItem(cache_key);
+    if (
+      stored_value === "auto" ||
+      stored_value === "light" ||
+      stored_value === "dark"
+    ) {
+      setTheme(stored_value);
+    } else {
+      setTheme("auto");
+    }
+    setFirstLoad(false);
+  }, []);
+
+  const switchTheme = useCallback((newTheme: "auto" | "light" | "dark") => {
+    setTheme(newTheme);
+  }, []);
+
+  const colourThemeContext = useMemo(
+    () => ({
+      theme,
+      switchTheme,
+    }),
+    [theme, switchTheme]
+  );
+  return (
+    <ColourThemeContext.Provider value={colourThemeContext}>
+      {children}
+    </ColourThemeContext.Provider>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ColourThemeContextProvider } from './controller/context/ColourThemeContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ColourThemeContextProvider>
+      <App />
+    </ColourThemeContextProvider>
   </React.StrictMode>
 );
 

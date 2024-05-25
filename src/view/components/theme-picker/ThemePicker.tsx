2dce6718e35380aeb59da96932d06d4dd4654eda
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2024-05-26 08:29:43 +1200

    [style]: Theme picker styling.
    Update styling for theme picker.

--- src/view/components/theme-picker/ThemePicker.tsx
+++ src/view/components/theme-picker/ThemePicker.tsx
@@ -1,17 +1,47 @@
-import { useContext } from "react";
 import "./ThemePicker.css";
+import { useContext } from "react";
 import { ColourThemeContext } from "../../../controller/context/ColourThemeContext";
 import { SunIcon } from "../../assets/icons/SunIcon";
 import { MoonIcon } from "../../assets/icons/MoonIcon";
 import { SunMoonIcon } from "../../assets/icons/SunMoonIcon";
 
 export const ThemePicker = () => {
-    const {switchTheme} = useContext(ColourThemeContext)
+  const { theme, switchTheme } = useContext(ColourThemeContext);
+  const selectedClass = (value: "auto" | "dark" | "light"): string => {
+    if (value === theme) {
+      return " selected";
+    }
+    return "";
+  };
   return (
     <div className="themes">
-      <button className="theme-button" onClick={() => {switchTheme("auto")}}><SunMoonIcon />Auto</button>
-      <button className="theme-button" onClick={() => {switchTheme("light")}}><SunIcon />Light</button>
-      <button className="theme-button" onClick={() => {switchTheme("dark")}}><MoonIcon />Dark</button>
+      <button
+        className={"theme-button" + selectedClass("auto")}
+        onClick={() => {
+          switchTheme("auto");
+        }}
+      >
+        <SunMoonIcon />
+        System
+      </button>
+      <button
+        className={"theme-button" + selectedClass("light")}
+        onClick={() => {
+          switchTheme("light");
+        }}
+      >
+        <SunIcon />
+        Light
+      </button>
+      <button
+        className={"theme-button" + selectedClass("dark")}
+        onClick={() => {
+          switchTheme("dark");
+        }}
+      >
+        <MoonIcon />
+        Dark
+      </button>
     </div>
   );
 };
